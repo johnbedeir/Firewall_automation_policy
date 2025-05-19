1113bab731e942ece84422b97e5101377060649a
John Bedeir <johnbedeir@Mac.lan>
2025-05-19 15:36:02 -0400

    update firewall rule to port 22

--- firewall.tf
+++ firewall.tf
@@ -4,8 +4,32 @@ resource "google_compute_firewall" "allow_http" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["80"]
   }
 
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-2"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-ip"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.145.83.115"]
 }
