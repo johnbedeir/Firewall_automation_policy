edf2a556dbbcc916cfbc1e6e4782ed66fa3c935a
John Bedeir <johnbedeir@Mac.lan>
2025-05-19 15:46:31 -0400

    [skip ci]

--- firewall.tf
+++ firewall.tf
@@ -9,27 +9,3 @@ resource "google_compute_firewall" "allow_http" {
 
   source_ranges = ["0.0.0.0/0"]
 }
-
-resource "google_compute_firewall" "allow_port" {
-  name    = "allow-port"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["35.145.83.115"]
-}
